a6458bdc815b62de52bbe50902d9f3b6bea924e3
Kevin Stine <ikaikastine@gmail.com>
2018-09-24 21:22:16 -0700

    Made a few minor adjustments

--- myapp/app.js
+++ myapp/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var jquery = require('jquery')
+var bootstrap = require('bootstrap')
+
 var app = express();
 
 // view engine setup
